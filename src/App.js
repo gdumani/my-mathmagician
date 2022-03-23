441f650c8dfb9d078d93cb1ee3225338b6516b75
gdumani <gdumani@gmail.com>
2022-03-23 14:45:46 -0600

    fix navbar, add currentquotes and calc,home and quote pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-  <div>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/calc">Calculator</Link>
-    </li>
-    <li>
-      <Link to="/quote">Quotes</Link>
-    </li>
+  <div className="nav">
+    <h1>Math Magician</h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/calc">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/quote">Quotes</Link>
+      </li>
+    </ul>
   </div>
 );
 export default NavBar;
