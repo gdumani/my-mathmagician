d7045b5e9cff3c06da5f4a2adb4466161e547f39
gdumani <gdumani@gmail.com>
2022-03-23 14:57:26 -0600

    import pages in APP,  fix references

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import {
   Route,
 } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Home from './pages/home';
+import Calc from './pages/calc';
+import Quote from './pages/quote';
 
 export default function App() {
   return (
