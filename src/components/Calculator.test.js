bc80a5b94b4523746d80fd095d0e17152267cd66
gdumani <gdumani@gmail.com>
2022-03-24 15:36:51 -0600

    add CurrenQuotes test

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,5 +1,5 @@
 import {
-  render, fireEvent, waitFor, screen,
+  render, fireEvent, screen,
 } from '@testing-library/react';
 import React from 'react';
 import Calculator from './Calculator';
--- src/components/CurrentQuotes.test.js
+++ src/components/CurrentQuotes.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from './NavBar';
+import CurrentQuote from './CurrentQuotes';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<NavBar />, div);
+  ReactDOM.render(<CurrentQuote />, div);
 });
